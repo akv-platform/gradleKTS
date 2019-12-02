1a6b4f0827a55f53e5541c4ebbbfb6098ce514e7
AndreyMaslennikov <v-andrem@microsoft.com>
2019-12-02 13:50:14 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
+android {
+    compileSdkVersion(Apps.compileSdk)
+    buildToolsVersion(Apps.buildToolsVersion)
+    defaultConfig {
+        minSdkVersion(Apps.minSdk)
+        targetSdkVersion(Apps.targetSdk)
+        versionCode = Apps.versionCode
+        versionName = Apps.versionName
+        multiDexEnabled = true
+        setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
+        resConfigs("en")
+        ndk {
+            abiFilters("armeabi-v7a", "x86", "arm64-v8a", "x86_64")
+        }
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Libs.kotlin)
+    implementation(Libs.appcompat)
+    implementation(Libs.constraintLayout)
+    testImplementation(TestLibs.junit)
+}
