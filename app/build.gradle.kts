3b35fc7dc4742c61c62c5dabd21c2758073aae6b
AndreyMaslennikov <v-andrem@microsoft.com>
2019-12-02 17:12:25 +0300

    Remove unnecessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,6 @@ android {
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(Libs.kotlin)
     implementation(Libs.appcompat)
     implementation(Libs.constraintLayout)
